87ccc3a82376711c0d78aaac26db211916acb970
Heinrich37 <belforthenrich2004@gmail.com>
2025-05-07 21:47:52 -0400

    ..

---
+++ Pyton/for;in.py
@@ -0,0 +1,7 @@
+nomes = ["pedro", "joao", "leticia"]
+for i in nomes:
+    print(i)
+    if i == "joao":
+        break
+    
+    
---
+++ Pyton/tabuada.py
@@ -0,0 +1,3 @@
+i=int(input("Digite o numero a ser multiplicado: "))
+for x in range(11):
+    print(f"{i}*{x}= {i*x}")
