8793d7176b9fb35d5f1ff99be625061a499ed085
Heinrich37 <belforthenrich2004@gmail.com>
2025-05-08 21:56:44 -0400

    terminar

--- Pyton/tabuada.py
+++ Pyton/tabuada.py
@@ -1,3 +1,28 @@
-i=int(input("Digite o numero a ser multiplicado: "))
-for x in range(11):
-    print(f"{i}*{x}= {i*x}")
+# i=int(input("Digite o numero a ser multiplicado: "))
+# for x in range(11):
+#     print(f"{i}*{x}= {i*x}")
+# =================================================================================
+# numero1, numero2 = map(int, input("Digite o dois numeros: ").split())
+# for n in range(numero1+1, numero2):
+#     print(n, end=' ')
+# for n in range(numero1+1, numero2):
+#     print(n)
+#==================================================================================
+# soma = 0
+# numero = int(input("Informe a quantidade de numeros voce quer digitar: "))
+# for i in range(numero):
+#     n1 = int(input("Informe o numero: "))
+
+menor = 9999999999    
+maior = -999999999
+soma = 0
+numero = int(input("Informe a quantidade de números que você quer digitar: "))
+
+for i in range(numero):
+    n1 = int(input("Informe o número: "))
+    soma += n1
+
+    if i <= menor:
+        menor  = 1
+    
+    
---
+++ Pyton/tiime.py
@@ -0,0 +1,12 @@
+# time = ["Palmeiras", "Flamengo", "São Paulo"]
+# for x in time :
+#     if x == "Palmeiras":
+#         print("1-", x)
+#     if x == "Flamengo":
+#         print("2-", x)
+#     if x == "São Paulo":
+#         print("3-", x)
+time = ["Palmeiras", "Flamengo", "São Paulo"]
+for i in range(len(time)):
+    print(f"{i+1}- {time[i]}")
+
